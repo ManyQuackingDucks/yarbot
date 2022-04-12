dcf893f17049c487d10ea95b864b8d8c3c665dcc
A Duck <duckquack123321@protonmail.com>
2022-04-12 18:47:39 +0000

    Finished first version just need to test

--- src/constant.rs
+++ src/constant.rs
@@ -1,6 +1,18 @@
-pub fn get_avatar_url(user_id: &str) -> String{
+pub const REQUEST_LIMIT: usize = 40;
+pub const RETRY_LIMIT: usize = 100;
+pub const PLACE_ID: &str = ""; //TODO: Add place id
+pub const CAPTAIN_ID: &str = ""; //TODO: Add captain id
+pub fn get_avatar_url(user_id: &str) -> String {
     format!("https://www.roblox.com/headshot-thumbnail/image?userId={user_id}&width=48&height=48&format=png")
 }
-pub fn get_id_url(user_name: &str) -> String{
+pub fn get_id_url(user_name: &str) -> String {
     format!("https://api.roblox.com/users/get-by-username?username={user_name}")
+}
+
+pub fn get_game_instances(place_id: &str, i: usize) -> String {
+    format!("www.roblox.com/games/getgameinstancesjson?placeId={place_id}&startIndex={i}")
+}
+
+pub fn get_join_url(place_id: &str, guid: &str) -> String {
+    format!("https://www.roblox.com/home?placeid={place_id}&gameid={guid}")
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,108 @@
+use std::env;
+
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::{Args, CommandResult};
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+use serenity::{
+    async_trait, client::bridge::gateway::GatewayIntents, framework::standard::StandardFramework,
+    model::gateway::Ready,
+};
+#[group]
+#[commands(raid)]
+struct Basic;
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
 #[allow(dead_code)]
 mod constant;
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let framework = StandardFramework::new().configure(|c| {
+        c.with_whitespace(true)
+            .delimiters(vec![", ", ","])
+            .prefix("~yar")
+    });
+    let mut client = Client::builder(&token)
+        .event_handler(Handler)
+        .framework(framework)
+        .intents(GatewayIntents::all())
+        .await
+        .expect("Err creating client");
+    client.start_autosharded().await.unwrap();
+}
+
+#[command("raid")]
+#[allowed_roles("Captian")]
+#[only_in(guilds)]
+#[sub_commands(start, end)]
+async fn raid(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
+    msg.reply(&ctx.http, "~yar raid <start, end>").await?;
+    Ok(())
+}
+#[command("start")]
+#[allowed_roles("Captian")]
+#[only_in(guilds)]
+#[description("Start a raid")]
+/// Unwraps are allowed on the serde_json stuff because I can guarrentee that as long as roblox is avaiable
+/// and there is one person in the game they will not panic
+async fn start(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
+    let mut mes = msg.reply(&ctx, "Starting a raid").await?;
+    let mut vec = vec![];
+    let client = reqwest::Client::new();
+    for i in 0..constant::REQUEST_LIMIT {
+        let url = constant::get_game_instances(constant::PLACE_ID, i * 10);
+        let res = client.get(url).send().await?;
+
+        let res_json: serde_json::Value = serde_json::from_str(&res.text().await?)?;
+        for x in res_json.as_object().unwrap()["Collection"]
+            .as_array()
+            .unwrap()
+        {
+            for y in x.as_object().unwrap()["CurrentPlayers"].as_array().unwrap() {
+                if y.as_object().unwrap()["Id"].as_str().unwrap() == constant::CAPTAIN_ID {
+                    vec.push(x.clone()); //Should really only be one
+                }
+            }
+        }
+    }
+    if vec.is_empty() {
+        mes.edit(&ctx.http, |m| {
+            m.content("Yar! I could not find the captain online!")
+        })
+        .await?;
+    } else {
+        let join_url = constant::get_join_url(
+            constant::PLACE_ID,
+            vec[0].as_object().unwrap()["Guid"].as_str().unwrap(),
+        );
+        mes.edit(&ctx.http, |m| {
+            m.content(format!(
+                "@everyone Yar! Rading time (open url in web browser to join captian):\n{join_url}"
+            ))
+        })
+        .await?;
+        mes.pin(&ctx.http).await?;
+    }
+    Ok(())
+}
+
+#[command("end")]
+#[allowed_roles("Captian")]
+#[only_in(guilds)]
+#[description("End a raid")]
+async fn end(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
+    msg.reply(&ctx.http, "@everyone Yar! The raid is now over!")
+        .await?
+        .pin(&ctx.http)
+        .await?;
+    Ok(())
 }
