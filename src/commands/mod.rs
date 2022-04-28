a7e1a06af0399c65b59f3feff73ed9ede1250014
A Duck <duckquack123321@protonmail.com>
2022-04-28 17:57:19 +0000

    generialize checks

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -14,67 +14,26 @@ pub mod raid;
 #[check]
 #[name = "bot_command"]
 pub async fn bot_command(ctx: &Context, msg: &Message) -> Result<(), Reason> {
-    let client = ctx.data.read().await;
-    let sqlite = client.get::<SqliteClient>().unwrap();
-    let mut conn = sqlite.connect().unwrap();
-    let channel_id = msg.channel_id.to_string();
-    let config_channel_id = match crate::config::get("bot_channel_id", &mut conn) {
-        Ok(channel_id) => channel_id,
-        Err(e) => {
-            return Err(Reason::Log(e.to_string()));
-        }
-    };
-    match config_channel_id {
-        Some(config_channel_id) => {
-            if channel_id == config_channel_id {
-                return Ok(());
-            }
-            return Err(Reason::User(
-                "You can't use this command in this channel".to_string(),
-            ));
-        }
-        None => {
-            return Err(Reason::User("Please set the bot channel".to_string()));
-        }
-    }
+    channel_check(ctx, msg.channel_id.to_string(),"bot_channel_id").await
 }
 
 #[check]
 #[name = "raid_command"]
 pub async fn raid_command(ctx: &Context, msg: &Message) -> Result<(), Reason> {
-    let client = ctx.data.read().await;
-    let sqlite = client.get::<SqliteClient>().unwrap();
-    let mut conn = sqlite.connect().unwrap();
-    let channel_id = msg.channel_id.to_string();
-    let config_channel_id = match crate::config::get("raid_channel_id", &mut conn) {
-        Ok(channel_id) => channel_id,
-        Err(e) => {
-            return Err(Reason::Log(e.to_string()));
-        }
-    };
-    match config_channel_id {
-        Some(config_channel_id) => {
-            if channel_id == config_channel_id {
-                return Ok(());
-            }
-            return Err(Reason::User(
-                "You can't use this command in this channel".to_string(),
-            ));
-        }
-        None => {
-            return Err(Reason::User("Please set the raid channel".to_string()));
-        }
-    }
+    channel_check(ctx, msg.channel_id.to_string(),"raid_channel_id").await
 }
 
 #[check]
 #[name = "point_command"]
 pub async fn point_command(ctx: &Context, msg: &Message) -> Result<(), Reason> {
+    channel_check(ctx, msg.channel_id.to_string(),"point_channel_id").await
+}
+
+async fn channel_check(ctx: &Context, channel_id: String, config_name: &str) -> Result<(), Reason>{
     let client = ctx.data.read().await;
     let sqlite = client.get::<SqliteClient>().unwrap();
     let mut conn = sqlite.connect().unwrap();
-    let channel_id = msg.channel_id.to_string();
-    let config_channel_id = match crate::config::get("point_channel_id", &mut conn) {
+    let config_channel_id = match crate::config::get(config_name, &mut conn) {
         Ok(channel_id) => channel_id,
         Err(e) => {
             return Err(Reason::Log(e.to_string()));
@@ -85,12 +44,12 @@ pub async fn point_command(ctx: &Context, msg: &Message) -> Result<(), Reason> {
             if channel_id == config_channel_id {
                 return Ok(());
             }
-            return Err(Reason::User(
+            Err(Reason::User(
                 "You can't use this command in this channel".to_string(),
-            ));
+            ))
         }
         None => {
-            return Err(Reason::User("Please set the bot channel".to_string()));
+            Err(Reason::User("Please set the bot channel".to_string()))
         }
     }
 }
